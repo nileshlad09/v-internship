d03ceb53226e513160929b6421e7251ecebe25b1
nilesh <nileshlad871@gmail.com>
2023-01-21 20:53:36 +0530

    home page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {Route, Routes } from "react-router-dom";
 import  Home  from "./Components/Home/Home";
 import  Admin  from "./Components/Admin/Admin";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <>
+    <Navbar/>
     <Routes>
       <Route path="/admin" element={<Admin/>}/>
       <Route path="/" element={<Home/>}/>
