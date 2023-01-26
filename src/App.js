57e8be89795ec6ea52989b74e1ef22e33589a1ba
nilesh <nileshlad871@gmail.com>
2023-01-26 14:35:19 +0530

    Login, forgot password, studenthome added

--- src/App.js
+++ src/App.js
@@ -7,16 +7,21 @@ import StudentData from './Components/StudentData/StudentData'
 import {Switch,Route} from 'react-router-dom';
 import View from "./Components/View/View";
 import ExcelData from "./Components/ExcelData/ExcelData";
+import StudentaddData from "./Components/SrudentAdddata/StudentaddData";
+import Login from "./Components/Login/Login";
+import Forgotpassword from "./Components/Login/Forgotpassword";
 function App() {
   return (
-
     <div>
      <Navbar/>
      <Switch>
-       <Route  exact path='/' component={Admin}></Route>
+       <Route exact path='/' component={Admin}></Route>
        <Route exact path ='/StudentData' component={StudentData}></Route>
        <Route exact path ='/View/:fname/:roll/:Sem/:branch/:organization/:email/:start/:end/:id/:domain' component={View}></Route>
        <Route exact path ='/ExcelData' component={ExcelData}></Route>
+       <Route exact path ='/studenthome' component={StudentaddData}></Route>
+       <Route exact path ='/login' component={Login}></Route>
+       <Route exact path ='/forgotpassword' component={Forgotpassword}></Route>
      </Switch>
     </div>
 
