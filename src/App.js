05bdc2fb5336549853fa65871ebfe3366b227f02
Nikhil <adharenikhil@gmail.com>
2023-01-27 22:50:49 +0530

    create DataEnter folder

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Navbar from './Components/Navbar/Navbar'
 import Dashboard from './Pages/Dashboard'
 import AdminLogin from './Components/Adminlogin/AdminLogin'
 import View from './Pages/View'
+import DataEnter from './Components/DataEnter/DataEnter'
+import AddInternship from './Components/DataEnter/AddInternship';
 function App() {
   return (
     <div>
@@ -13,6 +15,8 @@ function App() {
        <Route exact path='/' component={AdminLogin} ></Route>
        <Route exact path='/admin' component={Dashboard} ></Route>
        <Route exact path='/admin/view' component={View} ></Route>
+       <Route exact path='/DataEnter' component={DataEnter} ></Route>
+       <Route exact path='/AddInternship' component={AddInternship} ></Route>
      </Switch>
     </div>
 
