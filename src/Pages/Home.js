67123e92d0d4cb4c0dde9815130539fead7952c2
Nikhil Adhare <adharenikhil@gmail.com>
2023-04-15 22:27:54 +0530

    some changed has been done in home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './home.css'
+// import logo from './vitlogo.png'
 const Home = () => {
 
 
@@ -10,14 +11,15 @@ const Home = () => {
   return (
     <div className='images'>
        <div className='homePage'>
-      <h3 className="homePageHeadingmain">V-Internship</h3>
-      <h4 className="homePageHeading">Internship Portal of <span style={{ color: "red" }}>Vidyalankar Institute of Technology(VIT)</span>, Mumbai</h4>
+      <h3 className="homePageHeadingmain"><span style={{color:"blue"}}>V</span><span style={{color:"blue"}}>I</span>n<span style={{color:"blue"}}>T</span>ernship </h3>
+     {/* <div className='homePageHeadingmain'><img scr={logo} ></img></div>  */}
+      <h4 className="homePageHeading">Internship Portal of <span style={{ color: "blue" }}>Vidyalankar Institute of Technology(VIT)</span>, Mumbai</h4>
       <div className="homeOptions">
-        <Link to={`/addinternship/1`}><div className="homeOption_box">
+        <Link to={`/addinternship/1`} style={{textDecoration:"none"}}><div className="homeOption_box">
           <p className='homeOption_title_2'>Add Internship Data</p>
         </div>
         </Link>
-        <Link to={`/adminlogin`}> <div className="homeOption_box">
+        <Link to={`/adminlogin`} style={{textDecoration:"none"}}> <div className="homeOption_box">
           <p className='homeOption_title_2'>Admin Login  </p>
         </div>
         </Link>
