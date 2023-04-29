8890292257c24a03a32725fa54393383da088d4c
Sanyog Jain <sanyogjain5678@gmail.com>
2023-04-29 23:10:42 +0530

    Move keys to environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -9,12 +9,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId
+  apiKey: `${process.env.apiKey}`,
+  authDomain: `${process.env.authDomain}`,
+  projectId: `${process.env.projectId}`,
+  storageBucket: `${process.env.storageBucket}`,
+  messagingSenderId: `${process.env.messagingSenderId}`,
+  appId: `${process.env.appId}`
 };
 
 // Initialize Firebase
